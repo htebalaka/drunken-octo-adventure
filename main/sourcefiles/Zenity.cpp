2dcc7d9c77d1647bb8707baa2a38faf41add199e
Malcolm Gooding <goodingm@gmail.com>
2014-11-19 09:22:59 -0800

    work on zenity scripts
    will need to test these in the lab to make sure they're working correctly

--- main/sourcefiles/Zenity.cpp
+++ main/sourcefiles/Zenity.cpp
@@ -9,83 +9,83 @@ using namespace std;
 
 string Zenity::getUserText(string title, string text, string def)
 {
-   return getCmdOutput(
-         "zenity --entry "
-                "--title=\"" + title + "\" "
-                "--height=240 --width=360 "
-                "--text=\"" + text + "\" "
-                "--entry-text=\"" + def + "\"");
+  return getCmdOutput(
+      "zenity --entry "
+      "--title=\"" + title + "\" "
+      "--height=240 --width=360 "
+      "--text=\"" + text + "\" "
+      "--entry-text=\"" + def + "\"");
 }
 
 string Zenity::getTableEntry(string title, string text, vector<string> columns, vector<string> entries)
 {
-   string columnconcat = "";
-   for (auto i : columns)
-   {  columnconcat += "--column=\"" + i + "\" "; }
+  string columnconcat = "";
+  for (auto i : columns)
+  {  columnconcat += "--column=\"" + i + "\" "; }
 
-   string entriesconcat = "";
-   for (auto i : entries)
-   {  entriesconcat += i + " "; }
+  string entriesconcat = "";
+  for (auto i : entries)
+  {  entriesconcat += i + " "; }
 
-   return getCmdOutput(
-         "zenity --list "
-                "--title=\"" + title + "\" "
-                "--text=\"" + text + "\" "
-                + columnconcat + " " + entriesconcat);
+  return getCmdOutput(
+      "zenity --list "
+      "--title=\"" + title + "\" "
+      "--text=\"" + text + "\" "
+      + columnconcat + " " + entriesconcat);
 }
 
 string Zenity::getAnswer(string title, string text, string ok, string no)
 {
-   return getCmdOutput(
-         "zenity --question "
-                "--title=\"" + title + "\" "
-                "--text=\"" + text + "\" "
-                "--ok-label=\"" + ok + "\" "
-                "--cancel-label=\"" + no + "\" ");
+  return getCmdOutput(
+      "zenity --question "
+      "--title=\"" + title + "\" "
+      "--text=\"" + text + "\" "
+      "--ok-label=\"" + ok + "\" "
+      "--cancel-label=\"" + no + "\" ");
 }
 
 // run the specified command, capture the output 
 //    it produces and return it as a string
 // taken from the lab9 cppZen.cpp sourcecode
-string getCmdOutput(string cmd)
+string Zenity::getCmdOutput(string cmd)
 {
-   // string to store the final results in
-   string outputString;
+  // string to store the final results in
+  string outputString;
 
-   // stream to capture the command output
-   FILE *outpStream;
+  // stream to capture the command output
+  FILE *outpStream;
 
-   // maximum line length when capturing
-   //    command output
-   const int MaxLineLen = 128;
+  // maximum line length when capturing
+  //    command output
+  const int MaxLineLen = 128;
 
-   // storage for one line of captured output
-   char  outpLine[MaxLineLen];
+  // storage for one line of captured output
+  char  outpLine[MaxLineLen];
 
-   // redirect the standard output from thecommand 
-	cmd += " 2>&1";
+  // redirect the standard output from thecommand 
+  cmd += " 2>&1";
 
-   // run the command, piping the output to our stream
-      outpStream = popen(cmd.c_str(), "r");
-   if (outpStream) {
-      // capture the output, one line at a time,
-           //    appending it to our output string
-              while
-                 (!feof(outpStream))
-                 {
-                    if
-                       (fgets(outpLine,
-                              MaxLineLen,
-                              outpStream)
-                        !=
-                        NULL)
-                       {
-                          outputString
-                             +=
-                             outpLine;
-                       }
-                 }
-      pclose(outpStream);
-   }
-   return outputString;
+  // run the command, piping the output to our stream
+  outpStream = popen(cmd.c_str(), "r");
+  if (outpStream) {
+    // capture the output, one line at a time,
+    //    appending it to our output string
+    while
+      (!feof(outpStream))
+      {
+        if
+          (fgets(outpLine,
+                 MaxLineLen,
+                 outpStream)
+           !=
+           NULL)
+          {
+            outputString
+              +=
+              outpLine;
+          }
+      }
+    pclose(outpStream);
+  }
+  return outputString;
 }
