fb949bd8250611e95022673fe26663e5127d4b44
Malcolm Gooding <goodingm@gmail.com>
2014-11-21 08:45:53 -0800

    working on adding test stuff

--- main/headers/BoardGUI_hof.h
+++ main/headers/BoardGUI_hof.h
@@ -10,6 +10,9 @@ namespace BoardGUI_hof
    enum GAME_MODE { NEWGAME_TOP=-1, NEWGAME_BOTTOM=1, REG_GAME=0 };
    std::function<bool (int,int)> boundsCheck(GAME_MODE);
    char* flattenVec(std::vector< std::vector<char> >, bool);
+
+   std::pair< std::vector< std::vector<char> >, bool> un_flatten_vec(char*);
+   bool flatten_vec_identity(std::vector< std::vector<char> >, bool);
 }
 
 #endif
--- main/headers/Zenity.h
+++ main/headers/Zenity.h
@@ -9,7 +9,11 @@ namespace Zenity
 {
    std::string getCmdOutput(std::string);
    std::string getUserText(std::string, std::string, std::string);
-   std::string getTableEntry (std::string, std::string, std::vector<std::string>, std::vector< <std::vector<std::string> >);
+   std::string getTableEntry(
+           std::string title,
+           std::string text,
+           std::vector<std::string> columnNames,
+           std::vector< std::vector<std::string> > columnEntries);
    std::string getAnswer(std::string, std::string, std::string, std::string);
    std::string getCmdOutput(std::string);
 }
